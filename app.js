80275409f7a6e1659369224f235d13ffe6ed0a0c
Luis Izquierdo <a329630@uach.mx>
2021-05-26 14:42:07 -0600

    app.js funcionando

--- app.js
+++ app.js
@@ -3,28 +3,24 @@ const fs = require('fs');
 
 http.createServer((request,response)=>{
     console.log(request.url);
-    var file = request.url == '/' ? './matriculas.json' : `./${request.url}`;
-    fs.readFile(file,(err,data)=>{
-        if(err){
-            response.writeHead(404,{"Content-type":"text/plain"});
-            response.write("not found");
-            response.end();
-        }else{
-            const extension = file;
-            switch(extension){
-                case '329675.json':
-                    response.writeHead(200,{"Content-type":"application/json"});
-                    break;
-                case '329630.json':
-                    response.writeHead(200,{"Content-type":"application/json"});
-                    break;
-                case '329830.json':
-                    response.writeHead(200,{"Content-type":"application/json"});
-                    break;
+    let file = `.${request.url}.json`;
+
+    if(request.url == '/'){
+        response.writeHead(200, {"Content-Type":"text/plain"});
+        response.write("Pagina principal");
+        response.end();
+    } else{
+        
+        fs.readFile(file,(error,data)=>{
+            if(error){
+                response.writeHead(404,{"Content-type":"text/plain"});
+                response.write("not found");
+                response.end();
+            }else{
+                response.writeHead(200,{"Content-type":"application/json"});
+                response.write(data);
+                response.end();
             }
-            response.write(data);
-            response.end();
-        }
-    });
-    
+        });
+    }
 }).listen(8888);
