ac79c990d6a114487e665cc935b8d83b70f7639a
Luis Izquierdo <a329630@uach.mx>
2021-05-26 14:46:20 -0600

    Cambios menores

--- app.js
+++ app.js
@@ -3,18 +3,17 @@ const fs = require('fs');
 
 http.createServer((request,response)=>{
     console.log(request.url);
-    let file = `.${request.url}.json`;
+    let file = `./matriculas${request.url}.json`;
 
     if(request.url == '/'){
         response.writeHead(200, {"Content-Type":"text/plain"});
         response.write("Pagina principal");
         response.end();
     } else{
-        
         fs.readFile(file,(error,data)=>{
             if(error){
                 response.writeHead(404,{"Content-type":"text/plain"});
-                response.write("not found");
+                response.write("Not found");
                 response.end();
             }else{
                 response.writeHead(200,{"Content-type":"application/json"});
