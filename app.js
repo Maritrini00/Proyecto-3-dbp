5006fa7ac63c601847495453677c7e893f26902d
Maritrini Velázquez <a329675@uach.mx>
2021-05-26 12:36:04 -0600

    cambio en el server

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require ('http');
 const fs = require('fs');
 
-http.createServer((request,response) => {
+/* http.createServer((request,response) => {
     console.log(request.url);
     const file = request.url == '/' ? './WWW/index.html' : `./WWW${request.url}`;
 
@@ -70,4 +70,32 @@ http.createServer((request,response) => {
             }
         });
     }
+}).listen(8888); */
+
+http.createServer((request,response)=>{
+    console.log(request.url);
+    var file = request.url == '/' ? './matriculas.json' : `./${request.url}`;
+    fs.readFile(file,(err,data)=>{
+        if(err){
+            response.writeHead(404,{"Content-type":"text/plain"});
+            response.write("not found");
+            response.end();
+        }else{
+            const extension = file;
+            switch(extension){
+                case '329675.json':
+                    response.writeHead(200,{"Content-type":"application/json"});
+                    break;
+                case '329630.json':
+                    response.writeHead(200,{"Content-type":"application/json"});
+                    break;
+                case '329830.json':
+                    response.writeHead(200,{"Content-type":"application/json"});
+                    break;
+            }
+            response.write(data);
+            response.end();
+        }
+    });
+    
 }).listen(8888);
